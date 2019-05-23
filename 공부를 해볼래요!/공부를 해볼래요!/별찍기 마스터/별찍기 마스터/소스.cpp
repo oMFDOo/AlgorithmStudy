0cd6364ce94b93412b54edb2bb8f8bd5b414b898
oMFDOo <jinjoo021@naver.com>
2019-05-23 22:48:15 +0900

    1001 언어의 장벽 정답
    승미야 수업듣자

---
+++ 공부를 해볼래요!/공부를 해볼래요!/별찍기 마스터/별찍기 마스터/소스.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+int main(void)
+{
+	int testCase;
+	scanf("%d", &testCase);
+	while (testCase--) {
+		int a, b;
+		char text;
+		
+		scanf("%d %d", &a, &b);
+
+		if (a % 2 == 0) {
+			a += 1;
+		}
+
+		for (int y = 0; y < a; y++) {
+			if (a / 2 - b < 0) {
+				printf("No DAP\n");
+				break;
+			}
+			text = 'A' -1;
+			for (int x = 0; x < a; x++) {
+				if ((y < b || y >= a - b) || (x >= a - b || x < b)) {
+					if (x >= a/2+1) {
+						text--;
+						if (y < b || y >= a - b)
+							printf("%c", text);
+						else
+							printf("%c", text + 1);
+					}
+					else  {
+						text++;
+						printf("%c", text);
+					}
+				}
+				else {
+					printf(" ");
+				}
+			}
+			printf("\n");
+		}
+	}
+}
