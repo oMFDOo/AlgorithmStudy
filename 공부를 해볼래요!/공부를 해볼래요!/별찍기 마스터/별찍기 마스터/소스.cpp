aec9ff9d0d0a7cc3b4cd4a416a0457265706bce2
oMFDOo <jinjoo021@naver.com>
2019-05-23 22:50:17 +0900

    1000. 민규의 기만 정답
    민규야 자제하자

--- 공부를 해볼래요!/공부를 해볼래요!/별찍기 마스터/별찍기 마스터/소스.cpp
+++ 공부를 해볼래요!/공부를 해볼래요!/별찍기 마스터/별찍기 마스터/소스.cpp
@@ -6,36 +6,18 @@ int main(void)
 	scanf("%d", &testCase);
 	while (testCase--) {
 		int a, b;
-		char text;
-		
 		scanf("%d %d", &a, &b);
-
-		if (a % 2 == 0) {
-			a += 1;
-		}
-
 		for (int y = 0; y < a; y++) {
 			if (a / 2 - b < 0) {
-				printf("No DAP\n");
+				printf("Mental Damages\n");
 				break;
 			}
-			text = 'A' -1;
 			for (int x = 0; x < a; x++) {
 				if ((y < b || y >= a - b) || (x >= a - b || x < b)) {
-					if (x >= a/2+1) {
-						text--;
-						if (y < b || y >= a - b)
-							printf("%c", text);
-						else
-							printf("%c", text + 1);
-					}
-					else  {
-						text++;
-						printf("%c", text);
-					}
+					printf("@");
 				}
 				else {
-					printf(" ");
+					printf("O");
 				}
 			}
 			printf("\n");
