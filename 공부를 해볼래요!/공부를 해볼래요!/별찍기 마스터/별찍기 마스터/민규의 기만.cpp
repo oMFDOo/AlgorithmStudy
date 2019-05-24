9b8e8215fcdb71a2872ab06120ce7bb5eb9903c1
oMFDOo <jinjoo021@naver.com>
2019-05-24 14:30:13 +0900

    유클리드 호제법
    나중에 봐야지

--- 공부를 해볼래요!/공부를 해볼래요!/별찍기 마스터/별찍기 마스터/민규의 기만.cpp
+++ 공부를 해볼래요!/공부를 해볼래요!/별찍기 마스터/별찍기 마스터/민규의 기만.cpp
@@ -1,26 +1,26 @@
-#include <stdio.h>
-#pragma warning(disable:4996)
-int main(void)
-{
-	int testCase;
-	scanf("%d", &testCase);
-	while (testCase--) {
-		int a, b;
-		scanf("%d %d", &a, &b);
-		for (int y = 0; y < a; y++) {
-			if (a / 2 - b < 0) {
-				printf("NOA\n");
-				break;
-			}
-			for (int x = 0; x < a; x++) {
-				if ((y < b || y >= a - b) || (x >= a - b || x < b)) {
-					printf("@");
-				}
-				else {
-					printf("O");
-				}
-			}
-			printf("\n");
-		}
-	}
-}
+//#include <stdio.h>
+//#pragma warning(disable:4996)
+//int main(void)
+//{
+//	int testCase;
+//	scanf("%d", &testCase);
+//	while (testCase--) {
+//		int a, b;
+//		scanf("%d %d", &a, &b);
+//		for (int y = 0; y < a; y++) {
+//			if (a / 2 - b < 0) {
+//				printf("NOA\n");
+//				break;
+//			}
+//			for (int x = 0; x < a; x++) {
+//				if ((y < b || y >= a - b) || (x >= a - b || x < b)) {
+//					printf("@");
+//				}
+//				else {
+//					printf("O");
+//				}
+//			}
+//			printf("\n");
+//		}
+//	}
+//}
--- 공부를 해볼래요!/공부를 해볼래요!/별찍기 마스터/별찍기 마스터/언어의 장벽.cpp
+++ 공부를 해볼래요!/공부를 해볼래요!/별찍기 마스터/별찍기 마스터/언어의 장벽.cpp
@@ -1,42 +1,24 @@
 #include <stdio.h>
-#pragma warning(disable:4996)
-int main(void)
-{
+#pragma warning (disable : 4996)
+
+int main() {
+
 	int testCase;
 	scanf("%d", &testCase);
 	while (testCase--) {
-		int a, b;
-		char text;
+		char al;
+		
+		int size;
+		scanf("%d", &size);
 
-		scanf("%d %d", &a, &b);
-
-		if (a % 2 == 0) {
-			a += 1;
-		}
-
-		for (int y = 0; y < a; y++) {
-			if (a / 2 - b < 0) {
-				printf("NOA\n");
-				break;
-			}
-			text = 'A' - 1;
-			for (int x = 0; x < a; x++) {
-				if ((y < b || y >= a - b) || (x >= a - b || x < b)) {
-					if (x >= a / 2 + 1) {
-						text--;
-						if (y < b || y >= a - b)
-							printf("%c", text);
-						else
-							printf("%c", text + 1);
-					}
-					else {
-						text++;
-						printf("%c", text);
-					}
+		for (int y = 0; y < size; y++) {
+			al = 'A';
+			for (int x = 0; x < size; x++) {
+				if (y == x) {
+					al += y;
 				}
-				else {
-					printf(" ");
-				}
+				printf("%c", al);
+				al++;
 			}
 			printf("\n");
 		}
