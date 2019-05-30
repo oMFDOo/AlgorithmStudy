3ce75727c9cd898294f6e745e268d88c6ebcdbe7
oMFDOo <jinjoo021@naver.com>
2019-05-31 01:49:23 +0900

    Aoikazto 1074 진주의 샹들리에
    홀수도 사랑해달라!

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Aoikazto 1074 진주의 샹들리에.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int tC;
+	scanf("%d", &tC);
+	while(tC--) {
+		int S, T;
+		scanf("%d %d", &S, &T);
+		if (S % 2 != 0) {
+			S++;
+		}
+		if (T > S / 2) {
+			printf("No DAP!\n");
+		}
+		else {
+			for (int i = 0; i < S; i++) {
+				int c = 0;
+				for (int j = 0; j < i; j++) {
+					printf(" ");
+				}
+				for (int j = 0; j < (S-i)*2-1; j++) {
+					if (j > (S - i) * 2 - 2 - T || j < T ||i<T) {
+						if (j <= ((S - i) * 2 - 1) / 2) {
+							printf("%c", 'A' + (((S - i) * 2 - 1) / 2 - j));
+						}
+						else {
+							printf("%c", 'A' + j - ((S-i)*2-1)/2);
+						}
+						
+					}
+					else {
+						printf(" ");
+					}
+				}
+				printf("\n");
+			}
+			printf("\n");
+		}
+	}
+}
