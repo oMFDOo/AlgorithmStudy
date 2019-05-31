ecedcd97345c968f923ac5dfd0793fa47cbcc3e3
oMFDOo <jinjoo021@naver.com>
2019-06-01 02:23:48 +0900

    Lavida 1058

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1058.cpp
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+
+
+#define SCISSORS 1
+
+#define ROCK 2
+
+#define PAPER 3
+
+
+
+#define WIN 1
+
+#define LOSE -1
+
+#define DRAW 0
+
+
+
+int calculateRockScissorsPaper(int rockScissorsPaper1, int rockScissorsPaper2) {
+
+	if (rockScissorsPaper1 == SCISSORS) {
+
+		if (rockScissorsPaper2 == SCISSORS) {
+
+			return DRAW;
+
+		}
+
+		if (rockScissorsPaper2 == ROCK) {
+
+			return LOSE;
+
+		}
+
+		if (rockScissorsPaper2 == PAPER) {
+
+			return WIN;
+
+		}
+
+	}
+
+	if (rockScissorsPaper1 == ROCK) {
+
+		if (rockScissorsPaper2 == SCISSORS) {
+
+			return WIN;
+
+		}
+
+		if (rockScissorsPaper2 == ROCK) {
+
+			return DRAW;
+
+		}
+
+		if (rockScissorsPaper2 == PAPER) {
+
+			return LOSE;
+
+		}
+
+	}
+
+	if (rockScissorsPaper1 == PAPER) {
+
+		if (rockScissorsPaper2 == SCISSORS) {
+
+			return LOSE;
+
+		}
+
+		if (rockScissorsPaper2 == ROCK) {
+
+			return WIN;
+
+		}
+
+		if (rockScissorsPaper2 == PAPER) {
+
+			return DRAW;
+
+		}
+
+	}
+
+}
+
+
+
+// expectedRockScissorsPaperResult = 0, 1, -1
+
+int calculatePercent(int RL, int RR, int HL, int HR, int expectedRockScissorsPaperResult) {
+
+	// RL vs HL
+
+
+
+	int caseCount = 0;
+
+
+
+	// WinRLHL = RL이 이기면 1, 지면 0
+
+
+
+	if (calculateRockScissorsPaper(RL, HL) == expectedRockScissorsPaperResult) {
+
+		caseCount += 1;
+
+	}
+
+	if (calculateRockScissorsPaper(RL, HR) == expectedRockScissorsPaperResult) {
+
+		caseCount += 1;
+
+	}
+
+	if (calculateRockScissorsPaper(RR, HL) == expectedRockScissorsPaperResult) {
+
+		caseCount += 1;
+
+	}
+
+	if (calculateRockScissorsPaper(RR, HR) == expectedRockScissorsPaperResult) {
+
+		caseCount += 1;
+
+	}
+
+
+
+	const int totalCaseCount = 4;
+
+	// 0, 1, 2, 3
+
+
+
+	// 퍼센트 계산 = (경우의 수) / (총 경우의 수) * 100
+
+	int percent = caseCount * 100 / totalCaseCount;
+
+
+
+	return percent;
+
+}
+
+
+
+int main() {
+
+	int TC;
+
+	scanf("%d", &TC);
+
+	while (TC--) {
+
+		// ::: 류주가 이길 / 비길 / 질 확률 :::  1. 가위 2. 바위 3. 보   :::
+
+		int RL, RR, HL, HR;
+
+		int win, draw, lose;
+
+
+
+		scanf("%d %d %d %d", &RL, &RR, &HL, &HR);
+
+
+
+		win = calculatePercent(RL, RR, HL, HR, WIN);
+
+		draw = calculatePercent(RL, RR, HL, HR, DRAW);
+
+		lose = calculatePercent(RL, RR, HL, HR, LOSE);
+
+
+
+		printf("%d %d %d\n", win, draw, lose);
+
+	}
+
+}
--- Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2422.cpp
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2422.cpp
@@ -1,57 +1,107 @@
-#include <stdio.h>
-#pragma warning (disable : 4996)
-
-//int main() {
+//#include <stdio.h>
+//#pragma warning (disable : 4996)
+//
+//int main()
+//{	
 //	int TC;
 //	scanf("%d", &TC);
 //	while (TC--) {
-//		int price, save, sub, day = 0, MF;
+//		int price, save, sub;
 //		scanf("%d %d", &price, &save);
 //		sub = price - save;
-//		MF = sub % 217000;
+//
+//		int dayPrices[] = {
+//		3 * 7000,
+//		3 * 7000,
+//		3 * 7000,
+//		3 * 7000,
+//		3 * 7000,
+//		8 * 7000,
+//		8 * 7000,
+//		};
+//
+//		auto day = 0;
 //		if (sub > 0) {
-//			day += (sub / 217000) * 7;
-//			if ( MF > 105000) {
-//				if (MF > 161000) {
-//					day++;
-//				}
-//				day += 6;
+//			auto weeks = sub / 217000;
+//			auto weekdaySub = sub % 217000;
+//
+//			auto weekday = 0;
+//			while (weekdaySub > 0) {
+//				weekdaySub -= dayPrices[weekday];
+//				weekday += 1;
 //			}
-//			else {
-//				if (MF != 0) {
-//					day++;
-//				}
-//				day += (MF) / 21000;
-//			}
+//
+//			day += weeks * 7 + weekday;
 //		}
 //		printf("%d\n", day);
 //	}
 //}
 
-int main() {
-	int TC;
-	scanf("%d", &TC);
-	while (TC--){
-		int price, save, sub, day = 0;
-		scanf("%d %d", &price, &save);
-		sub = price - save;
-		while (1) {
-			if (sub <= 0)
-				break;
-			for (int i = 0; i < 5 && sub > 0; i++) {
-				sub -= 21000;
-				day++;
-			}
-			if (sub <= 0)
-				break;
-			for (int i = 0; i < 2 && sub > 0; i++) {
-				sub -= 56000;
-				day++;
-			}
-			if (sub <= 0)
-				break;
-		}
-		printf("%d\n", day);
-		
-	}
-}
+//int main() {
+//    int TC;
+//    scanf("%d", &TC);
+//    while (TC--) {
+//        int price, save, sub, day = 0;
+//        scanf("%d %d", &price, &save);
+//        sub = price - save;
+//        if (sub > 0) {
+//            // 일주일 처리
+//            day += (sub / 217000) * 7;
+//
+//            // 남은 돈
+//            sub = sub % 217000;
+//
+//            // 5일 초과로 일 안해도 됩니까?
+//            if (sub <= 105000) {
+//                day += sub / 21000;
+//                sub = sub % 21000;
+//
+//                // 애매하게 하루 일해야합니까?
+//                if (sub > 0) {
+//                    day += 1;
+//                }
+//            }
+//            else {
+//                // 5일 일했다고 치고
+//                sub -= 105000;
+//                day += 5;
+//
+//                // 토일 수당으로 계산 한번 더 들어갑니다.
+//                day += sub / 56000;
+//                sub = sub % 56000;
+//
+//                if (sub % 56000 > 0) {
+//                    day += 1;
+//                }
+//            }
+//        }
+//        printf("%d\n", day);
+//    }
+//}
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//	while (TC--){
+//		int price, save, sub, day = 0;
+//		scanf("%d %d", &price, &save);
+//		sub = price - save;
+//		while (1) {
+//			if (sub <= 0)
+//				break;
+//			for (int i = 0; i < 5 && sub > 0; i++) {
+//				sub -= 21000;
+//				day++;
+//			}
+//			if (sub <= 0)
+//				break;
+//			for (int i = 0; i < 2 && sub <= 0; i++) {
+//				sub -= 56000;
+//				day++;
+//			}
+//			if (sub <= 0)
+//				break;
+//		}
+//		printf("%d\n", day);
+//		
+//	}
+//}
