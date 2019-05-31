b433c262ce66f393be152cdd9bd88481dd75ff93
oMFDOo <jinjoo021@naver.com>
2019-05-31 23:21:21 +0900

    Lavida 1022
    언젠가 짧게 다시 해보자!

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1022.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int TC;
+	scanf("%d", &TC);
+	while (TC--) {
+		int size;
+		scanf("%d", &size);
+
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size - y -1; x++) {
+				printf(" ");
+			}
+			int width = y*2+1;
+			for (int x = 0; x < y*2+1; x++) {
+				if (x < (width / 2)) {
+					printf("%d", 1 + x);
+				}
+				else {
+					printf("%d", width - x);
+				}
+			}
+			printf("\n");
+		}
+		for (int y = 0; y < size-1; y++) {
+			for (int x = 0; x <= y; x++) {
+				printf(" ");
+			}
+			for (int x = 0; x < (size - y) * 2 -3; x++) {
+				if (x < ((size-y) * 2 -1)/2) {
+					printf("%d", 1 + x);
+				}
+				else {
+					printf("%d", (size-y)*2-3-x);
+				}
+				
+			}
+			printf("\n");
+		}
+	}
+}
--- Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2766.cpp
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2766.cpp
@@ -1,25 +1,25 @@
-#include <stdio.h>
-#pragma warning (disable : 4996)
-
-int main() {
-	int num;
-	scanf("%d", &num);
-	for (int y = 0; y < num; y++) {
-		for (int x = 0; x < num - y - 1; x++) {
-			printf(" ");
-		}
-		for (int x = 0; x < y * 2 + 1; x++) {
-			printf("*");
-		}
-		printf("\n");
-	}
-	for (int y = 0; y < num - 1; y++) {
-		for (int x = 0; x <= y; x++) {
-			printf(" ");
-		}
-		for (int x = 0; x < (num - y) * 2 - 3; x++) {
-			printf("*");
-		}
-		printf("\n");
-	}
-}
+//#include <stdio.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int num;
+//	scanf("%d", &num);
+//	for (int y = 0; y < num; y++) {
+//		for (int x = 0; x < num - y - 1; x++) {
+//			printf(" ");
+//		}
+//		for (int x = 0; x < y * 2 + 1; x++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (int y = 0; y < num - 1; y++) {
+//		for (int x = 0; x <= y; x++) {
+//			printf(" ");
+//		}
+//		for (int x = 0; x < (num - y) * 2 - 3; x++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
