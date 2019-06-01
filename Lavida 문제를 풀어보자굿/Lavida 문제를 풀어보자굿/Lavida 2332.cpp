52630fa9bbf20f8a4a9a723d0093f718eb588d1f
oMFDOo <jinjoo021@naver.com>
2019-06-02 00:59:14 +0900

    Lavida 2332

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2332.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int TC;
+	scanf("%d", &TC);
+	while (TC--) {
+		char OX[55];
+		int len, index = 0;
+		scanf("%s", &OX[0]);
+		len = strlen(OX);
+		for (int i = 0; i < len; i++) {
+			if (OX[i] == 'o' || OX[i] == 'x') {
+				index = i;
+				break;
+			}
+		}
+		for (int i = index%2 ; i < len; i += 2) {
+			OX[i] = OX[index];
+		}
+		for (int i = (index+1)%2; i < len; i += 2) {
+			if (OX[index] == 'x') {
+				OX[i] = 'o';
+			}
+			else {
+				OX[i] = 'x';
+			}
+		}
+		printf("%s\n", OX);
+	}
+}
