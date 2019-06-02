df5defd2b8352305f42d3cee77daac1b78d1f749
oMFDOo <jinjoo021@naver.com>
2019-06-02 23:40:33 +0900

    Lavida 2796

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2796.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int TC;
+	scanf("%d", &TC);
+	while (TC--) {
+		int SaRam[7] = {0}, sum = 0, min, max, temp, cnt = 0;
+		for (int i = 0; i < 7; i++) {
+			scanf("%d", &SaRam[i]);
+			sum += SaRam[i];
+		}
+		for (int i = 0; i < 7; i++) {			// ¸ÚÂð ³»°¡ Á¤·ÄÇÏ´Â ±¸°£
+			for (int j = 0; j+1 < 7-i; j++) {	// ¡Ú				   ¡Ú
+				if (SaRam[j] > SaRam[j+1]) {	// ¡Ù				   ¡Ù
+					temp = SaRam[j];			// ¡Ú		 ^)^ 	   ¡Ú
+					SaRam[j] = SaRam[j+1];		// ¡Ù		  V 	   ¡Ù
+					SaRam[j+1] = temp;			// ¡Ú				   ¡Ú
+				}
+			}
+		}
+		if (SaRam[1] - SaRam[5] > 4) {
+			sum -= SaRam[0] + SaRam[1] + SaRam[5] + SaRam[6];
+			printf("%d\n", sum/3);
+		}
+		else {
+			sum -= SaRam[0] + SaRam[6];
+			printf("%d\n", sum/5);
+		}
+		/*for (int i = 0; i < 7; i++) {
+			printf("%d ", SaRam[i]);
+		}
+		printf("\n");*/
+	}
+}
