69c63e05ea99b8fae74d2996bbf1257077e1b227
oMFDOo <jinjoo021@naver.com>
2019-06-03 16:24:28 +0900

    Lavida 2162 복권 긁기

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2162 복권 긁기.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int TC;
+	scanf("%d", &TC);
+	while (TC--) {
+		int LOTTO[7], Num[6], cnt = 0, bonus = 0;
+		for (int i = 0; i < 7; i++) {	// 로또번호 받기
+			scanf("%d", &LOTTO[i]);
+		}
+		for (int i = 0; i < 6; i++) {	// 내 로또 번호
+			scanf("%d", &Num[i]);
+		}
+		for (int i = 0; i < 6; i++) {	// 로또 번호 비교
+			for (int j = 0; j < 6; j++) {
+				if (LOTTO[i] == Num[j]) {
+					cnt++;
+				}
+				if (LOTTO[6] == Num[j]) {
+					bonus = 1;
+				}
+			}
+		}
+		switch (cnt) {
+			case 6: printf("1\n");
+				break;
+			case 5: ;
+				break;
+			case 4: printf("4\n");
+				break;
+			case 3: printf("5\n");
+				break;
+			default : printf("Fail\n");
+				break;
+		}
+		if (cnt == 5) {
+			if (bonus) {
+				printf("2\n");
+			}
+			else {
+				printf("3\n");
+			}
+		}
+	}
+}
