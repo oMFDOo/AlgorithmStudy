3956921115d77d41f1134ac609c89e19483693f3
oMFDOo <jinjoo021@naver.com>
2019-06-06 20:19:14 +0900

    Lavida 1067 그들의 족구

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1067 그들의 족구.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int TC;
+	scanf("%d", &TC);
+	while (TC--) {
+		char SimPan[25], GU[3] = "IN";
+		int in = 0, out = 0, EJin = 0, EJout = 0;
+		for (int i = 0; i < 8; i++) {
+			scanf("%s", SimPan);
+			if (strcmp(SimPan, GU)) {
+				if (i == 0 || i == 1) {
+					EJout++;
+				}
+				else {
+					out++;
+				}
+			}
+			else {
+				if (i == 0 || i == 1) {
+					EJin++;
+				}
+				else {
+					in++;
+				}
+			}
+		}
+		//printf("%d %d %d %d\n", EJin, EJout, in, out);
+		if (EJin == 2) {
+			printf("IN\n");
+		}
+		else if (EJout == 2) {
+			printf("OUT\n");
+		}
+		else if (in>out) {
+			printf("IN\n");
+		}
+		else if (out>in) {
+			printf("OUT\n");
+		}
+		else if (out == in) {
+			printf("AGAIN\n");
+		}
+	}
+}
