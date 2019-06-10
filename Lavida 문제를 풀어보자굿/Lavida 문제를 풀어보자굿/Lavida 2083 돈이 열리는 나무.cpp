d2ad83a4943a3c3662f0da2f22ed8066b5c731ba
oMFDOo <jinjoo021@naver.com>
2019-06-10 13:43:29 +0900

    Lavida 2083 돈이 열리는 나무

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2083 돈이 열리는 나무.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int TC;
+	scanf("%d", &TC);
+	while (TC--) {
+		double size;
+		int c = 1;
+		scanf("%lf", &size);
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size - y - 1; x++) {
+				printf(" ");
+			}
+			for (int x = 0; x < y * 2 + 1; x++) {
+				if (x < (y * 2 + 1) / 2+1) {
+					printf("%.lf", size-x);
+					c = 1;
+				}
+				else {
+					printf("%.lf", size-y+c);
+					c++;
+				}
+			}
+			printf("\n");
+		}
+		for (int y = 0; y < size-1; y++) {
+			for (int x = 0; x < y+1; x++) {
+				printf(" ");
+			}
+			for (int x = 0; x < (size-y) * 2 - 3; x++) {
+				if (x < ((size - y) * 2 - 3) / 2) {
+					printf("%.lf", size - x);
+					c = 3;
+				}
+				else {
+					printf("%d", y+c);
+					c++;
+				}
+			}
+			printf("\n");
+		}
+	}
+}
