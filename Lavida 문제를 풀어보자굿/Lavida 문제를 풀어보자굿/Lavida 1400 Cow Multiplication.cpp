01f182252ddfeafc8329de44475b07908c327ee7
oMFDOo <jinjoo021@naver.com>
2019-06-10 14:13:13 +0900

    Laivda 1092 직각삼각형의 넓이 구하기

--- Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1400 Cow Multiplication.cpp
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1400 Cow Multiplication.cpp
@@ -1,19 +1,19 @@
-#include <stdio.h>
-#pragma warning (disable : 4996)
-
-int main() {
-	unsigned long long A, B, C, D, sum = 0;
-	scanf("%lld %lld", &A, &B);
-	C = A;
-	
-	while(A) {
-		C = A%10;
-		D = B;
-		while (D) {
-			sum += D%10*C;
-			D /= 10;
-		}
-		A /= 10;
-	}
-	printf("%lld\n", sum);
-}
+//#include <stdio.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	unsigned long long A, B, C, D, sum = 0;
+//	scanf("%lld %lld", &A, &B);
+//	C = A;
+//	
+//	while(A) {
+//		C = A%10;
+//		D = B;
+//		while (D) {
+//			sum += D%10*C;
+//			D /= 10;
+//		}
+//		A /= 10;
+//	}
+//	printf("%lld\n", sum);
+//}
