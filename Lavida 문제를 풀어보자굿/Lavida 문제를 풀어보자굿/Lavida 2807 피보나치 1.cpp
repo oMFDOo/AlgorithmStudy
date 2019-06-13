7dffa6fb266d68aac51edc5d16345949ad781092
oMFDOo <jinjoo021@naver.com>
2019-06-13 23:31:04 +0900

    Lavida 2807 피보나치

--- Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2807 피보나치 1.cpp
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2807 피보나치 1.cpp
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#pragma warning (disable : 4996)
-
-int main() {
-	int TC;
-	scanf("%d", &TC);
-	while (TC--) {
-		int n,i,a=0, b=1, c=1;
-		scanf("%d", &n);
-		for (i = 0; i < n-1; i++) {
-			a += c;
-			c = b;
-			b = a;
-		}
-			printf("%d\n", b);
-	}
-}
+//#include <stdio.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//	while (TC--) {
+//		int n,i,a=0, b=1, c=1;
+//		scanf("%d", &n);
+//		for (i = 0; i < n-1; i++) {
+//			a += c;
+//			c = b;
+//			b = a;
+//		}
+//			printf("%d\n", b);
+//	}
+//}
