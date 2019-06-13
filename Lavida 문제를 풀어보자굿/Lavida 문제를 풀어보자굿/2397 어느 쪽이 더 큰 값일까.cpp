3b2b4ca53d66520a9cf27afb6f2375ba270b831c
oMFDOo <jinjoo021@naver.com>
2019-06-13 16:40:43 +0900

    2397 어느 쪽이 더 큰 값일까

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/2397 어느 쪽이 더 큰 값일까.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int TC;
+	scanf("%d", &TC);
+	while (TC--) {
+		int a, b, c, d;
+		double r1, r2;
+		scanf("%d %d %d %d", &a, &b, &c, &d);
+		r1 = pow(a, b);
+		r2 = pow(c, d);
+		if (r1 > r2) {
+			printf("%d^%d\n", a, b);
+		}
+		else if (r1 < r2) {
+			printf("%d^%d\n", c, d);
+		}
+		else {
+			printf("SAME\n");
+		}
+	}
+}
