a19dcc3ff9b91be08b37ed57d4490c1574fb5179
oMFDOo <jinjoo021@naver.com>
2019-06-13 16:33:50 +0900

    Lavida 1732 샤우팅윤

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1732 샤우팅윤.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int TC, T=1;
+	scanf("%d", &TC);
+	while (TC--) {
+		int s;
+		scanf("%d", &s);
+		printf("Case #%d:\n", T);
+		for (int y = 0; y < s*2-1; y++) {
+			for (int x = 0; x <= y; x++) {
+				if (y < y / 2)
+					printf("*");
+				else
+					if (x < s*2-1-y)
+						printf("*");
+			}
+			printf("\n");
+		}
+		printf("\n");
+		T++;
+	}
+}
