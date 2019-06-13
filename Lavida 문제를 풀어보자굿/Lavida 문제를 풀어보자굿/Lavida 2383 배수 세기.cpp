9a55071c6457232e3d7d918928a419931050bca7
oMFDOo <jinjoo021@naver.com>
2019-06-14 02:44:25 +0900

    Lavida 2383 배수 세기

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2383 배수 세기.cpp
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//#include <math.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int a, b;
+//	double rlt;
+//	scanf("%d %d", &a, &b);
+//	if (b % 2 == 0)
+//		rlt = 1.0*a*(1 + b)*(b / 2);
+//	else
+//		rlt = 1.0*a*b*((b + 1) / 2);
+//	printf("%.lf", rlt);
+//}
