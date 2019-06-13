53a9b7cc2b5455736aa3cac2955f4f546553a508
oMFDOo <jinjoo021@naver.com>
2019-06-14 01:42:40 +0900

    Lavida 1081 Root of the Problem

--- Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1081 Root of the Problem.cpp
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1081 Root of the Problem.cpp
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <math.h>
-#pragma warning (disable : 4996)
-
-int main() {
-	int TC;
-	scanf("%d", &TC);
-	while (TC--) {
-		double N, A, A_, B, C;
-		scanf("%lf %lf", &B, &N);
-		C = B;
-		B = pow(B, 1.0/N);
-		A = fabs(C- pow(round(B),N));
-		A_ = fabs(C - pow((int)B, N));
-		A > A_ ? printf("%d\n", (int)B) : printf("%.lf\n", round(B));
-	}
-}
+//#include <stdio.h>
+//#include <math.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//	while (TC--) {
+//		double N, A, A_, B, C;
+//		scanf("%lf %lf", &B, &N);
+//		C = B;
+//		B = pow(B, 1.0/N);
+//		A = fabs(C- pow(round(B),N));
+//		A_ = fabs(C - pow((int)B, N));
+//		A > A_ ? printf("%d\n", (int)B) : printf("%.lf\n", round(B));
+//	}
+//}
