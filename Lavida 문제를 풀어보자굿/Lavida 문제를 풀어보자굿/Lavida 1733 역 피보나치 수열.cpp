0c5e2ef05d46c47bfbf5269fd4f89e7fe0c79228
oMFDOo <jinjoo021@naver.com>
2019-06-14 01:59:16 +0900

    Lavida 1733 역 피보나치 수열

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1733 역 피보나치 수열.cpp
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	long long n, i, a=0, b=1, c=1, num[99] = {0,1};
+//	scanf("%lld", &n);
+//	for (i = 0; i < n; i++) {
+//		a += c;
+//		c = b;
+//		b = a;
+//		num[i + 2] = a;
+//	}
+//	for (i = n-1; i >= 0; i--) {
+//		printf("%lld\n", num[i]);
+//	}
+//}
