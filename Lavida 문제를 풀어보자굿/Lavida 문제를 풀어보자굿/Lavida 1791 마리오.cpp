0a546894ef59e578ffaf3446e541bc61de87a7c0
oMFDOo <jinjoo021@naver.com>
2019-06-14 01:42:28 +0900

    Lavida 1791 마리오

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1791 마리오.cpp
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//#include <math.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int A = 10, sum = 0, a, min = 10000, rlt, DAP = 0;
+//	while (A--) {
+//		scanf("%d", &a);
+//		sum += a;
+//		rlt = abs(100 - sum);
+//		if (min >= rlt) {
+//			min = rlt;
+//			DAP = sum;
+//		}
+//	}
+//	printf("%d\n", DAP);
+//}
