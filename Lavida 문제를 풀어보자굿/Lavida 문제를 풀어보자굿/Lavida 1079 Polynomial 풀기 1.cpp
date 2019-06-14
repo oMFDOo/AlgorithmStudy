ce8467e22ac1e24471b04fd26fc6c5532e855d81
oMFDOo <jinjoo021@naver.com>
2019-06-14 11:35:25 +0900

    Lavida 1079 Polynomial 풀기 1

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1079 Polynomial 풀기 1.cpp
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//#include <math.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//	while (TC--) {
+//		int turn, x, a, sum=0;
+//		scanf("%d %d", &turn, &x);
+//		while (x--) {
+//			scanf("%d", &a);
+//			sum += a * pow(turn, x);
+//		}
+//		printf("%d\n", sum);
+//	}
+//}
