8987d48a92a88467c509a69104f4c3244922ae1d
oMFDOo <jinjoo021@naver.com>
2019-06-16 03:11:09 +0900

    Lavida 2017 Bitwise Counting

--- Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2017 Bitwise Counting.cpp
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 2017 Bitwise Counting.cpp
@@ -1,18 +1,18 @@
-#include<stdio.h>
-int main()
-{
-	int a = 0, t = 256;
-	while (t--) {
-		int a_, num[8];
-		a_ = a;
-		for (int i = 0; i < 8; i++) {
-			num[7 - i] = a_ % 2;
-			a_ /= 2;
-		}
-		for (int i = 0; i < 8; i++) {
-			printf("%d", num[i]);
-		}
-		printf("\n");
-		a++;
-	}
-}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0, t = 256;
+//	while (t--) {
+//		int a_, num[8];
+//		a_ = a;
+//		for (int i = 0; i < 8; i++) {
+//			num[7 - i] = a_ % 2;
+//			a_ /= 2;
+//		}
+//		for (int i = 0; i < 8; i++) {
+//			printf("%d", num[i]);
+//		}
+//		printf("\n");
+//		a++;
+//	}
+//}
