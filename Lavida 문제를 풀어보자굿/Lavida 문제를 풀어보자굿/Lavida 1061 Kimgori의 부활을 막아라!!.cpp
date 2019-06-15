b47b26145c9f7f6354bc9cec9afda979512db65a
oMFDOo <jinjoo021@naver.com>
2019-06-16 02:01:48 +0900

    Update Lavida 1061 Kimgori의 부활을 막아라!!.cpp

--- Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1061 Kimgori의 부활을 막아라!!.cpp
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1061 Kimgori의 부활을 막아라!!.cpp
@@ -1,29 +1,25 @@
-#include <stdio.h>
-#pragma warning (disable : 4996)
-
-int main() {
-	int TC;
-	scanf("%d", &TC);
-	while (TC--) {
-		int a, b, num[100], cnt = 0;
-		scanf("%d %d", &a, &b);
-		for (int i = 1; i <= a; i++) {
-			scanf("%d", &num[i]);
-		}
-		int temp;
-		for (int i = 1; i <= a; i++) {
-			for (int j = 1; j + 1 <= a - i+1; j++) {
-				if (num[j] > num[j + 1]) {
-					temp = num[j];
-					num[j] = num[j + 1];
-					num[j + 1] = temp;
-				}
-			}
-		}
-		for (int i = 1; i <= a; i++) {
-			printf("%d ", num[i]);
-		}
-		printf("\n");
-		printf("%d\n", num[b]);
-	}
-}
+//#include <stdio.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//	while (TC--) {
+//		int a, b, num[100], cnt = 0;
+//		scanf("%d %d", &a, &b);
+//		for (int i = 1; i <= a; i++) {
+//			scanf("%d", &num[i]);
+//		}
+//		int temp;
+//		for (int i = 1; i <= a; i++) {
+//			for (int j = 1; j + 1 <= a - i+1; j++) {
+//				if (num[j] > num[j + 1]) {
+//					temp = num[j];
+//					num[j] = num[j + 1];
+//					num[j + 1] = temp;
+//				}
+//			}
+//		}
+//		printf("%d\n", num[b]);
+//	}
+//}
