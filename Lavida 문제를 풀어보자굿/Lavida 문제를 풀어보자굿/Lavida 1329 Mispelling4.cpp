56f47bea110e5a55614371455aa381a073a80748
oMFDOo <jinjoo021@naver.com>
2019-06-16 05:13:26 +0900

    Lavida 1329 Mispelling4

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1329 Mispelling4.cpp
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+//#include <string.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int TC, cnt = 1;
+//	scanf("%d", &TC);
+//
+//	while (TC--) {
+//		int T;
+//		char input[81] = { 0 };
+//		scanf("%d %s", &T, input);
+//
+//		printf("%d ", cnt);
+//		cnt++;
+//		int len = strlen(input);
+//		for (int i = 0; i < len; ++i) {
+//			if (i != T - 1) {
+//				printf("%c", input[i]);
+//			}
+//		}
+//		printf("\n");
+//	}
+//}
