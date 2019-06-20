2630fb8e261b7da0b8ae5cc409e5b9e862092123
oMFDOo <jinjoo021@naver.com>
2019-06-20 23:02:07 +0900

    Aoikazto 1089 3n+1 문제

--- Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Aoikazto 1089 3n+1 문제.cpp
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Aoikazto 1089 3n+1 문제.cpp
@@ -1,40 +1,38 @@
-#include <stdio.h>
-#include <limits.h>
-#pragma warning (disable : 4996)
-
-int main() {
-	int TC;
-	scanf("%d", &TC);
-	while (TC--) {
-		int i, j, cnt_M = 1, min = INT_MAX, max = 0;
-		scanf("%d %d", &i, &j);
-		if (i > j) {
-			int temp = i;
-			i = j;
-			j = temp;
-		}
-		for (int t = i; t <= j; t++) {
-			cnt_M = 1;
-			int num = t;
-			while (num != 1) {
-				if (num % 2 == 1) {
-					printf("È¦¼ö ÀÓ´Ï´ç : %d \n", num);
-					num = 3*num+1;
-					cnt_M++;
-				}
-				else {
-					printf("Â¦¼ö ÀÓ´Ï´ç : %d \n", num);
-					num /= 2;
-					cnt_M++;
-				}
-			}
-			if (min > cnt_M) {
-				min = cnt_M;
-			}
-			if (max < cnt_M) {
-				max = cnt_M;
-			}
-		}
-		printf("%d %d\n", max, min);
-	}
-}
+//#include <stdio.h>
+//#include <limits.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//	while (TC--) {
+//		int i, j, cnt_M = 1, min = INT_MAX, max = 0;
+//		scanf("%d %d", &i, &j);
+//		if (i > j) {
+//			int temp = i;
+//			i = j;
+//			j = temp;
+//		}
+//		for (int t = i; t <= j; t++) {
+//			cnt_M = 1;
+//			int num = t;
+//			while (num != 1) {
+//				if (num % 2 == 1) {
+//					num = 3*num+1;
+//					cnt_M++;
+//				}
+//				else {
+//					num /= 2;
+//					cnt_M++;
+//				}
+//			}
+//			if (min > cnt_M) {
+//				min = cnt_M;
+//			}
+//			if (max < cnt_M) {
+//				max = cnt_M;
+//			}
+//		}
+//		printf("%d %d\n", max, min);
+//	}
+//}
