6c9cfd9d97441167aef514371eca6745db9680b8
oMFDOo <jinjoo021@naver.com>
2019-06-23 02:13:02 +0900

    Aoikazto 1021 암호화 알고리즘2

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Aoikazto 1021 암호화 알고리즘2.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning (disable : 4996)
+
+int main()
+{
+	int TC;
+	scanf("%d", &TC);
+	while (TC--) {
+		char Data[1025] = { 0 }, Keyword[513] = { 0 }, Password[1025] = { 0 };
+		scanf("%s %s", Data, Keyword);
+
+		int LenData, LenPassword, CntLen = 0, TurnCnt = 0;
+
+		LenData = strlen(Data);
+		LenPassword = strlen(Keyword);
+
+		for (int y = 0; y < LenData; y++) {
+			if (strlen(Keyword) != CntLen) {
+				Password[TurnCnt++] = Keyword[CntLen++] ^ Data[y];
+			}
+			else {
+				CntLen = 0;
+				Password[TurnCnt++] = Keyword[CntLen++] ^ Data[y];
+			}
+		}
+		printf("%s\n", Password);
+	}
+}
