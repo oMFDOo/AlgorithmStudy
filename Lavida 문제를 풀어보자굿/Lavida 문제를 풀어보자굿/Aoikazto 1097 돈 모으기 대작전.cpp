b2bdd21520f7379b46e8a411ff224c4ff38ea0e2
oMFDOo <jinjoo021@naver.com>
2019-06-29 00:41:26 +0900

    Aoikazto 1097 돈 모으기 대작전

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Aoikazto 1097 돈 모으기 대작전.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int sum = 0;
+		printf("============================\n");
+		printf("입금\t출금\t잔고\n");
+		printf("============================\n");
+	while (1) {
+		int money = 1, GG = 0;
+		while (1) {
+			fflush(stdin);
+			if (scanf("%d", &money) == 1) {
+				sum += money;
+				break;
+			}
+			else {
+				GG = 1;
+				break;
+			}
+		}
+		if (GG) {
+			printf("============================\n");
+			return 0;
+		}
+		if (money > 0) {
+			printf("%d\t\t%d\n", money, sum);
+		}
+		if (money < 0) {
+			printf("\t%d\t%d\n", money*-1, sum);
+		}
+	}
+}
