3996699247bf8445bd34913a38a22e811d796c00
oMFDOo <jinjoo021@naver.com>
2019-07-10 02:03:34 +0900

    Aoikazto 1098 FizzBuzz

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Aoikazto 1098 FizzBuzz.cpp
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//	while (TC--) {
+//	int number;
+//	scanf("%d", &number);
+//	if (number%3 == 0 && number%5 == 0) {
+//		printf("FizzBuzz\n");
+//	}
+//	else if (number%3==0) {
+//		printf("Fizz\n");
+//	}
+//	else if (number % 5 == 0) {
+//		printf("Buzz\n");
+//	}
+//	else {
+//		printf("%d\n", number);
+//	}
+//	}
+//}
