263e2f97885216f83f00ce42d8a4ceb4c1988781
oMFDOo <jinjoo021@naver.com>
2019-07-10 02:04:04 +0900

    BaekJoon 10828 스택

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/BaekJoon 10828 스택.cpp
@@ -0,0 +1,64 @@
+//#include <stdio.h>
+//#include <string.h>
+//#pragma warning (disable : 4996)
+//
+//int Hmm[10000], pos = 1;
+//
+//int CommandCheck(char Type[9]) {
+//	int PushInput;
+//
+//	if (!strcmp(Type, "push")) {
+//		scanf("%d", &PushInput);
+//		Hmm[pos++-1] = PushInput;
+//	}
+//	else if (!strcmp(Type, "pop")) {
+//		if (pos > 1) {
+//			printf("%d\n", Hmm[pos - 2]);
+//			Hmm[pos- 2] = 0;
+//			pos--;
+//		}
+//		else {
+//			printf("-1\n");
+//		}
+//	}
+//	else if (!strcmp(Type, "size")) {
+//		if (pos > 1) {
+//			printf("%d\n", pos - 1);
+//		}
+//		else {
+//			printf("0\n");
+//		}
+//	}
+//	else if (!strcmp(Type, "empty")) {
+//		if (pos > 1) {
+//			printf("0\n");
+//		}
+//		else {
+//			printf("1\n");
+//		}
+//	}
+//	else if (!strcmp(Type, "top")) {
+//		if (pos > 1) {
+//			printf("%d\n", Hmm[pos-2]);
+//		}
+//		else {
+//			printf("-1\n");
+//		}
+//	}
+//	/*for (int i = 0; i < pos -1; i++) {
+//		printf("%d ", Hmm[i]);
+//	}
+//	printf("pos °ª : %d", pos);
+//	printf("\n\n");*/
+//	return 0;
+//}
+//
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//	while (TC--) {
+//		char Command[9];
+//		scanf("%s", Command);
+//		CommandCheck(Command);
+//	}
+//}
