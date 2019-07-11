dfcf5006263220623cfa373cb04490bf52a4ce56
oMFDOo <jinjoo021@naver.com>
2019-07-11 11:13:38 +0900

    Lavida 1080 Transposing A Matrix

---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/Lavida 1080 Transposing A Matrix.cpp
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+//#include <string.h>
+//#pragma warning (disable : 4996)
+//
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//
+//	while (TC--) {
+//		int a, b, matrix[100][100];
+//		scanf("%d %d", &a, &b);
+//		for (int y = 0; y < a; y++) {
+//			for (int x = 0; x < b; x++) {
+//				scanf("%d", &matrix[y][x]);
+//			}
+//		}
+//		for (int y = 0; y < b; y++) {
+//			for (int x = 0; x < a; x++) {
+//				printf("%d ", matrix[x][y]);
+//			}
+//			printf("\n");
+//		}
+//	}
+//}
---
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/reverse_array_print.cpp
@@ -0,0 +1,28 @@
+//#include <stdio.h>
+//#pragma warning (disable : 4996)
+//
+//int size, num[5000];
+//
+//void reverse(int HiHi) {
+//	if (HiHi > size) {
+//		printf("ÇÏÇÏ! ³¡³µ´Ù!\n");
+//		return ;
+//	}
+//	else {
+//		reverse(HiHi+1);
+//		printf("%d %d\n", num[HiHi], HiHi);
+//		return ;
+//	}
+//}
+//
+//int main() {
+//	int TC;
+//	scanf("%d", &TC);
+//	while (TC--) {
+//		scanf("%d", &size);
+//		for (int i = 1; i <= size; i++) {
+//			num[i] = i;
+//		}
+//		reverse(1);
+//	}
+//}
