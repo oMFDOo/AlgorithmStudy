7dd1634ff0b93fea161aa6bba2df2e48c314f106
oMFDOo <jinjoo021@naver.com>
2019-10-10 06:55:14 +0900

    Tic Tac Toe
    draw완성

--- Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/틱택토게임만들기.cpp
+++ Lavida 문제를 풀어보자굿/Lavida 문제를 풀어보자굿/틱택토게임만들기.cpp
@@ -5,25 +5,30 @@
 #include <array>
 using namespace std;
 
-array <char, 10> square = { 'o','1','2','3','4','5','6','7','8','9' };
+array <char, 50> square = { 'o','1','2','3','4','5','6','7','8','9' };
 
 //print
 void printBoard();
 void printExplain();
 void printMain();
+void changeColor(int);
+void resetColor();
 
 int selectMode();
 void printTurn(int, char);
 void selectPlayer1();
 void selectPlayer2();
 
+bool draw();
+
 
 int main() {
-	bool startGame = true, gameMode = true;
+	
+	bool startGame = true;
 	while (startGame) {
-		//printMain();
-		// array<int, 5> test;
-		int mode;
+		bool  gameMode = true;
+		square = { 'o','1','2','3','4','5','6','7','8','9' };
+		int mode, turnCount = 0;
 		mode = selectMode();
 
 		if (mode == 1)
@@ -33,18 +38,28 @@ int main() {
 			cout << "아직 구현 안 했어염 ><\n";
 
 		else if (mode == 3)
-			while (true) {
+			while (gameMode) {
+				
 				printBoard();
 				cout << endl;
 				printTurn(1, 79);
 				selectPlayer1();
+				turnCount++;
+				if (turnCount > 8) {
+					gameMode = false;
+					startGame = draw();
+					if (!gameMode) {
+						break;
+					}
+					printBoard();
+				}
 				printBoard();
 				printTurn(2, 88);
 				selectPlayer2();
+				turnCount++;
 			}
 	}
 }
-
 void printMain()
 {
 	system("cls");
@@ -56,30 +71,105 @@ void printMain()
 	Sleep(800);
 }
 
-void selectPlayer2()
-{
-	string inputPlace;
-	cin >> inputPlace;
-
-	int place = atoi(inputPlace.c_str());
-	square[place] = 'X';
-
+bool draw() {
+	system("cls");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	cout << endl << endl;
+	cout << "_______________________________________________________________________________________________" << endl;
+	cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl << endl;
+	cout << " '      '             '                   '            '                      '         '" << endl;
+	cout << R"(            __________        ____________        ___________  '    ___  ___  ___)" << endl;
+	cout << R"(           /   _____  \  '   /  ________  \      /  ______   |     /  / /  / /  /)" << endl;
+	cout << R"(          /   /     \  \    /  /        |  |    /  /      |  |  ' /  / /  / /  /)" << endl;
+	cout << R"(    '    /   /      |  |   /  /_______ /  /    /  /_______|  |   /  / /  / /  /)" << endl;
+	cout << R"(        /   /      |  |   /  ________    /  ' /  _________/  |  /  / /  / /  /)" << endl;
+	cout << R"(       /   /   '  /  /   /  /        \  \    /  /        /  /  /  /_/  /_/  /)" << endl;
+	cout << R"(  '   /   /______/  /   /  /         |  |   /  /        /  /  |           /)" << endl;
+	cout << R"(      |____________/   /__/          |__|  /__/   '    /__/    \____/\____/)" << endl;
+	cout << "        '             '          '                            '                 '   " << endl;
+	cout << " '      '             '                   '            '                      '         '" << endl << endl;
+	cout << "_______________________________________________________________________________________________" << endl;
+	cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << endl;
+	while (true) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+		cout << "    \t\t1. 다시하기 \t 2.그만두기\n\n";
+		string inputRestart;
+		resetColor();
+		cin >> inputRestart;
+		int restart = atoi(inputRestart.c_str());
+		if (restart == 1) {
+			printBoard();
+			return true;
+		}
+		else if (restart == 2) {
+			return false;
+		}
+		else {
+			cout << "올바른 번호를 입력해주세요. \n\n";
+		}
+	}
+	
+	resetColor();
 }
 
 void selectPlayer1()
 {
-	string inputPlace;
-	cin >> inputPlace;
+	while (true) {
+		string inputPlace;
+		cin >> inputPlace;
+		int place = atoi(inputPlace.c_str());
 
-	int place = atoi(inputPlace.c_str());
-	square[place] = 'O';
-
+		if (place > 9) {
+			cout << "\r올바른 위치를 선택해주세요.\n\n";
+		}
+		else if (square[place] == 'O' || square[place] == 'X') {
+			cout << "\r이미 선택된 자리 입니다.\n";
+		}
+		else if (place == 0) {
+			cout << "\r올바른 위치를 선택해주세요.\n";
+		}
+		else {
+			square[place] = 'O';
+			return;
+		}
+	}
 }
+
+void selectPlayer2()
+{
+	while (true) {
+		string inputPlace;
+		cin >> inputPlace;
+		int place = atoi(inputPlace.c_str());
+		if (place > 9) {
+			cout << "\r올바른 위치를 선택해주세요.\n\n";
+		}
+		else if (square[place] == 'O' || square[place] == 'X') {
+			cout << "\r이미 선택된 자리 입니다.\n";
+		}
+		else if (place == 0) {
+			cout << "\r올바른 위치를 선택해주세요.\n\n";
+		}
+		else {
+			square[place] = 'X';
+			return;
+		}
+	}
+}
+
 
 void printTurn(int turn, char simbol)
 {
-
-	cout << "\tPlayer" << turn << " (" << simbol << ")의 차례입니다.";
+	if (turn == 1) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+	}
+	else if (turn == 2){
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+	}
+	cout << "\tPlayer" << turn;
+	resetColor();
+	cout << " (" << simbol << ")의 차례입니다.\n";
+	cout << "    원하는 위치를 입력해주세요 >> ";
 }
 
 // mode select
@@ -108,43 +198,103 @@ int selectMode()
 void printExplain()
 {
 	system("cls");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 014);
 	cout << "\n\t\t<< tic tac toe 게임설명 >>\n\n";
-	cout << "    1. Player1과 Player2는 번갈아가며 말을 둡니다.\n";
-	cout << "    2. Player1은 'O', Player2는 'X'로 표시됩니다.\n";
-	cout << "    3. 두 사람 중 가로, 세로, 대각선 중 하나라도 3개가 연결되면 이깁니다.\n";
-	cout << "    4. 두 사람 모두 연결에 실패하면 비깁니다.\n";
+	resetColor();
+	cout << "    > Player1과 Player2는 번갈아가며 말을 둡니다.\n";
+	cout << "    > Player1은 'O', Player2는 'X'로 표시됩니다.\n";
+	cout << "    > 두 사람 중 가로, 세로, 대각선 중 하나라도 3개가 연결되면 이깁니다.\n";
+	cout << "    > 두 사람 모두 연결에 실패하면 비깁니다.\n\n";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+	cout << "    \t2. 혼자하기 \t 3. 두 명에서 하기\n\n";
+	resetColor();
 }
 
 // draw game board
 void printBoard()
 {
 	system("cls");
-	cout << "\n\t★☆★☆★☆★☆★☆★\n";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
 	cout << "\t======================";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
 	cout << "\n\t  << Tic Tac Toe >>\n";
-	cout << "\t======================\n";
-	cout << "\t☆★☆★☆★☆★☆★☆\n\n";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	cout << "\t======================\n\n";
 
+	resetColor();
 	cout << "    Player 1 (O)  -  Player 2 (X)" << endl << endl;
 	cout << endl;
 	cout << "  ■■■■■■■■■■■■■■■■" << endl;
 	cout << "  ■\t\t\t\t■" << endl;
 	cout << "  ■\t\t\t\t■" << endl;
 	cout << "  ■\t     |     |     \t■" << endl;
-	cout << "  ■\t  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << "\t\t■" << endl;
+
+	cout << "  ■\t  ";
+	changeColor(1);
+	cout << square[1];
+	resetColor();
+	cout << "  |  ";
+	changeColor(2);
+	cout << square[2];
+	resetColor();
+	cout << "  |  ";
+	changeColor(3);
+	cout << square[3];
+	resetColor();
+	cout << "\t\t■" << endl;
 
 	cout << "  ■\t_____|_____|_____\t■" << endl;
 	cout << "  ■\t     |     |     \t■" << endl;
 
-	cout << "  ■\t  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << "\t\t■" << endl;
+	cout << "  ■\t  ";
+	changeColor(4);
+	cout << square[4];
+	resetColor();
+	cout << "  |  ";
+	changeColor(5);
+	cout << square[5];
+	resetColor();
+	cout << "  |  ";
+	changeColor(6);
+	cout << square[6];
+	resetColor();
+	cout << "\t\t■" << endl;
 
 	cout << "  ■\t_____|_____|_____\t■" << endl;
 	cout << "  ■\t     |     |     \t■" << endl;
 
-	cout << "  ■\t  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << "\t\t■" << endl;
+	cout << "  ■\t  ";
+	changeColor(7);
+	cout << square[7];
+	resetColor();
+	cout << "  |  ";
+	changeColor(8);
+	cout << square[8];
+	resetColor();
+	cout << "  |  ";
+	changeColor(9);
+	cout << square[9];
+	resetColor();
+	cout << "\t\t■" << endl;
 
 	cout << "  ■\t     |     |     \t■" << endl;
 	cout << "  ■\t\t\t\t■" << endl;
 	cout << "  ■\t\t\t\t■" << endl;
 	cout << "  ■■■■■■■■■■■■■■■■" << endl << endl;
+}
+
+void changeColor(int color) {
+	if (square[color] == 'O') {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+	}
+	else if (square[color] == 'X') {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+	}
+	else {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	}
+}
+
+void resetColor() {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 }
