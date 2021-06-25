2f76f31dec65616f7bf2b44640b5b63a624890e0
oMFDOo <jinjoo021@naver.com>
2021-06-25 14:28:06 +0900

    .

---
+++ JAVA 도전/tset/src/tset/set.java
@@ -0,0 +1,19 @@
+package tset;
+import java.util.Scanner;
+
+public class set {
+
+	public static void main(String[] args) {
+		 	
+	        
+	        
+	        
+            
+	        int b = 2;
+            int a = 2;
+            b = ++a + ++a;
+	       
+            System.out.println(a + "  " + b);
+	        
+	}
+}
